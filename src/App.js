6fa36dfc2af2d19233ded007a7100e45ab140b14
wesrog <wesrog@gmail.com>
2020-06-06 08:23:47 -0700

    Initial audio recording work
    Allows access to mic and start/stop controls to activate recording. When
    recording is complete, a list of recordings can be monitored
    individually.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from 'react';
+import './App.css'
 
-function App() {
+const Clip = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <audio controls src={props.src}></audio>
+  )
+}
+
+const ClipList = props => {
+  const clips = props.clips
+  const clipItems = clips.map(clip =>
+    <div><Clip src={clip} /></div>
+  )
+
+  return (
+    <div className="clips">{clipItems}</div>
+  )
+}
+
+const App = () => {
+  const [recorder, setRecorder] = useState(null)
+  const [isRecording, setIsRecording] = useState(false)
+  const [clips, setClips] = useState([])
+
+  useEffect(() => {
+    const constraints = { audio: true }
+    navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError)
+  }, [])
+
+  const onSuccess = (stream) => {
+    setRecorder(new MediaRecorder(stream))
+    console.log('success')
+  }
+
+  const onError = (err) => {
+    console.log('err:', err)
+  }
+
+  const start = () => {
+    setIsRecording(true)
+    recorder.start()
+
+    recorder.ondataavailable = e => {
+      const blob = new Blob([e.data], {type: 'audio/ogg; codecs=opus'})
+      const audioUrl = window.URL.createObjectURL(blob)
+      setClips(clips => [...clips, audioUrl])
+      console.log(clips)
+    }
+
+    recorder.onstop = e => {
+      console.log('data avail after recorder stopped')
+    }
+  }
+
+  const stop = () => {
+    setIsRecording(false)
+    recorder.stop()
+  }
+
+  return (
+    <main className="App">
+      {
+        isRecording
+          ? <button onClick={stop}>Stop</button>
+          : <button onClick={start}>Start</button>
+      }
+      <p>{isRecording ? 'Recording...' : 'Hit start to record'}</p>
+
+      <ClipList clips={clips} />
+    </main>
   );
 }
 
