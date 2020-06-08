aad48e8013ceaa25c760220a389670ef1bbe1284
wesrog <wesrog@gmail.com>
2020-06-08 14:00:33 -0700

    not proud

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useRef } from 'react';
-import {Room, WebSocketChannel, MucSignaling, MediaDomElement} from 'rtc-lib'
+import React, { useRef, useState } from 'react';
+import { Room, WebSocketChannel, MucSignaling, MediaDomElement } from 'rtc-lib'
 import './App.css'
 
 const App = () => {
   const audioRef = useRef(null)
+  const [broadcastRoomName, setBroadcastRoomName] = useState('w35w0r7d')
+  const [listenRoomName, setListenRoomName] = useState('w35w0r7d')
 
-  const channel = new WebSocketChannel('wss://easy.innovailable.eu/w35w0r7d')
+  const channel = new WebSocketChannel(`wss://easy.innovailable.eu/${broadcastRoomName}`)
   const signaling = new MucSignaling(channel)
   const opts = {
     stun: 'stun:stun.innovailable.eu'
   }
   const room = new Room(signaling, opts)
   room.on('peer_joined', peer => {
-    console.log('peer connected')
-    console.log(peer)
+    console.log('peer connected: ', peer)
     new MediaDomElement(audioRef.current, peer)
+    audioRef.current.controls = true
 
     peer.on('left', () => {
       console.log('peer left')
@@ -23,14 +25,24 @@ const App = () => {
 
   const broadcast = async () => {
     const constraints = { audio: true }
+    const channel = new WebSocketChannel(`wss://easy.innovailable.eu/${broadcastRoomName}`)
+    const signaling = new MucSignaling(channel)
+    const opts = {
+      stun: 'stun:stun.innovailable.eu'
+    }
+    const room = new Room(signaling, opts)
+    room.on('peer_joined', peer => {
+      console.log('peer connected: ', peer)
+      new MediaDomElement(audioRef.current, peer)
+      audioRef.current.controls = true
+
+      peer.on('left', () => {
+        console.log('peer left')
+      })
+    })
 
     try {
       await room.connect()
-    } catch(err) {
-      alert(err)
-    }
-
-    try {
       room.local.addStream(constraints)
     } catch(err) {
       alert(err)
@@ -38,8 +50,22 @@ const App = () => {
   }
 
   const listen = async () => {
-    console.log('listen')
-    console.log(room)
+    const channel = new WebSocketChannel(`wss://easy.innovailable.eu/${listenRoomName}`)
+    const signaling = new MucSignaling(channel)
+    const opts = {
+      stun: 'stun:stun.innovailable.eu'
+    }
+    const room = new Room(signaling, opts)
+    console.log('room: ', room)
+    room.on('peer_joined', peer => {
+      console.log('peer connected: ', peer)
+      new MediaDomElement(audioRef.current, peer)
+      audioRef.current.controls = true
+
+      peer.on('left', () => {
+        console.log('peer left')
+      })
+    })
     try {
       await room.connect()
     } catch(err) {
@@ -49,9 +75,18 @@ const App = () => {
 
   return (
     <main className="App">
-      <button onClick={broadcast}>Broadcast</button>
-      <button onClick={listen}>Listen</button>
-      <audio autoPlay ref={audioRef}></audio>
+      <div>
+        Room: <input type="text" onChange={(e) => setBroadcastRoomName(e.target.value)} value={broadcastRoomName} />
+        <button onClick={broadcast}>Broadcast</button>
+      </div>
+
+      <div>
+        Room: <input type="text" onChange={(e) => setListenRoomName(e.target.value)} value={listenRoomName} />
+        <button onClick={listen}>Listen</button>
+        <div>
+          <audio autoPlay ref={audioRef}></audio>
+        </div>
+      </div>
     </main>
   );
 }
